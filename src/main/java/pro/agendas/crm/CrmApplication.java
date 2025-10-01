6264377ecaf5e26f818739445536d0a64d28fbf3
Denis Giovan Marques <denis.marques@icolabora.com>
2025-10-01 16:09:03 -0300

    Initial commit

---
+++ src/main/java/pro/agendas/crm/CrmApplication.java
@@ -0,0 +1,13 @@
+package pro.agendas.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+
+}
